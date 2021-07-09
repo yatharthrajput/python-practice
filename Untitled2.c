7589e3148acb019d88073fd35846a2fc5134c713
Yatharth Rajput <66031538+yatharthrajput@users.noreply.github.com>
2021-07-10 00:34:12 +0530

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+
+
+    int i,j,n,s;
+
+     printf("Enter value of j:");
+    scanf("%d",&j);
+     printf("Enter value of n:");
+    scanf("%d",&n);
+     printf("Enter value of space");
+    scanf("%d",&s);
+    i=j/(n-s);
+          printf("%d",i);
+          getchar();
+}
+
+
